93a687cf6cc5f79df71f1366502b0487e0507cb3
gurjeet <gurjit.babrah@gmail.com>
2019-10-16 10:39:34 -0400

    sensing node touched bt the user

--- Berzerk-F19/GameScene.swift
+++ Berzerk-F19/GameScene.swift
@@ -18,8 +18,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
     
+    
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+         // GET THE POSITION WHERE THE MOUSE WAS CLICKED
+           // ---------------------------------------------
+           let mouseTouch = touches.first
+            if (mouseTouch == nil) {
+                return
+            }
+            let location = mouseTouch!.location(in: self)
+    
+            // WHAT NODE DID THE PLAYER TOUCH
+           // ----------------------------------------------
+            let nodeTouched = atPoint(location).name
+            print("Player touched: \(nodeTouched)")
+       }
+    
     // detect when collision occurs
     func didBegin(_ contact: SKPhysicsContact) {
+        
+        
         
     }
 }
